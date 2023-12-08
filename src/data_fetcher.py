f2f7b6d9d7af72032d991b0cb388dcd591f6e2d7
Ofek Basson <bas.ofek@gmail.com>
2023-12-08 10:57:10 +0200

    Works good for the first number, empty image is opened for the secondimage and after.

--- src/data_fetcher.py
+++ src/data_fetcher.py
@@ -18,37 +18,34 @@ class DataFetcher:
         self.numbersAndLocationExistanceArray = np.zeros((10, 4))
         self.digitsImagesCounterDictionary = {0:0, 1:0, 2:0, 3:0, 4:0, 5:0, 6:0, 7:0, 8:0, 9:0}
         self.tabsAndNumbersDictionary = {}
-        self.originalTab = 0
+        self.originalTab = self.driver.current_window_handle
 
 
     def createDatabase(self):
         self.fillDataAndSubmit()
-        self.originalTab = self.driver.current_window_handle
-        for i in range(10):
+        for i in range(3):
             number, imageURL = self.getURLAndStringOfNumberDisplayed()
             self.createNewTab(imageURL)
-            currentTab = self.driver.current_window_handle
-            self.tabsAndNumbersDictionary[currentTab] = number
+            # currentTab = self.driver.current_window_handle
+            # self.tabsAndNumbersDictionary[currentTab] = number
+            self.saveImagesOfSpecificNumber(imageURL, number)
             self.switchToMainTab()
-            time.sleep(1)
             self.refreshNumber()
-        self.createDatabaseFromOpenTabs()
-            
-        # self.createNumbersAndURLsDictionary()
-        # time.sleep(5)
-        # self.createDatabaseFromDictionary()
+        # self.createDatabaseFromOpenTabs()
         self.driver.close()
-    
-    # def createDatabaseFromDictionary(self):
-    #     for number, url in self.numbersAndURLToFetch.items():
-    #         for i in range(10):
-    #             self.saveImagesOfSpecificNumber(url, number)
+
+    # def createDatabaseFromOpenTabs(self):
+    #     for window_handle in self.driver.window_handles:
+    #         if window_handle != self.originalTab:
+    #             self.driver.switch_to.window(window_handle)
+    #             currentURL = self.driver.current_url
+    #             displayedNumber = self.tabsAndNumbersDictionary[window_handle]
+    #             self.saveImagesOfSpecificNumber(currentURL, displayedNumber)
 
     def switchToMainTab(self):
         for window_handle in self.driver.window_handles:
-            if window_handle != self.originalTab:
+            if window_handle == self.originalTab:
                 self.driver.switch_to.window(window_handle)
-                break
     
     def createNewTab(self, imageURL):
         self.driver.switch_to.new_window('tab')
@@ -58,12 +55,7 @@ class DataFetcher:
         numbersImage = self.driver.find_element(By.XPATH, '//*[@id="ContentUsersPage_RadCaptcha1_CaptchaImageUP"]')
         imageURL = numbersImage.get_attribute("src")
         number = input('What is the displayed number?')
-        self.driver.switch_to.new_window('tab')
-        self.driver.get(imageURL)
         return number, imageURL
-    
-    def createDatabaseFromOpenTabs(self):
-        return
     
     def fillDataAndSubmit(self):
         self.driver.get("https://nadlan.taxes.gov.il/svinfonadlan2010/startpageNadlanNewDesign.aspx?ProcessKey=cb2fc2f5-08db-4099-9d1e-8eb9f6bcda42")
@@ -84,16 +76,6 @@ class DataFetcher:
         transactionType = self.driver.find_element(By.XPATH, '//*[@id="ContentUsersPage_DDLMahutIska"]')
         submitButton = self.driver.find_element(By.XPATH, '//*[@id="ContentUsersPage_btnHipus"]')
         return cityInput, propertyType, transactionType, submitButton
-
-    # def createNumbersAndURLsDictionary(self):
-    #     for i in range(1):
-    #         numbersImage = self.driver.find_element(By.XPATH, '//*[@id="ContentUsersPage_RadCaptcha1_CaptchaImageUP"]')
-    #         imageURL = numbersImage.get_attribute("src")
-    #         displayedNumber = input('What is the displayed number?')
-    #         self.driver.switch_to.new_window('tab')
-    #         self.driver.get("https://nadlan.taxes.gov.il/svinfonadlan2010/startpageNadlanNewDesign.aspx?ProcessKey=cb2fc2f5-08db-4099-9d1e-8eb9f6bcda42")
-    #         self.numbersAndURLToFetch[displayedNumber] = imageURL
-            # self.refreshNumber()
             
         
     def refreshNumber(self):
@@ -101,13 +83,14 @@ class DataFetcher:
         refreshLink.click()
 
     def saveImagesOfSpecificNumber(self, imageURL, number):
-        self.driver.get(imageURL)
-        myScreenshot = pyautogui.screenshot(region=(806, 614, 180, 48))
-        imagePath = f'/Users/wpqbswn/Desktop/Ofek/8200-learning/NadlanCaptchaNumbersClassification/Data/{number}.png'
-        myScreenshot.save(imagePath)
-        self.splitImageToFourDigits(imagePath, number)
-        os.remove(imagePath)
-        time.sleep(2)
+        for i in range(10):
+            self.driver.get(imageURL)
+            myScreenshot = pyautogui.screenshot(region=(806, 614, 180, 48))
+            imagePath = f'/Users/wpqbswn/Desktop/Ofek/8200-learning/NadlanCaptchaNumbersClassification/Data/{number}.png'
+            myScreenshot.save(imagePath)
+            self.splitImageToFourDigits(imagePath, number)
+            os.remove(imagePath)
+            time.sleep(2)
 
 
     def splitImageToFourDigits(self, imagePath, number):
@@ -143,7 +126,6 @@ class DataFetcher:
                 self.digitsImagesCounterDictionary[fourthDigit] += 1
             
 
-    # def deleteImage(self, imageToDelete):
 
         
 
